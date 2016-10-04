b499576096d4c328f4ba9a1c14f1ba8442699c0c
robinroyer <ktyroby@hotmail.fr>
2016-10-04 09:07:18 -0400

    [init] adding lab src

---
+++ src/probleme1/QuadraticSpacePerfectHashing.java
@@ -0,0 +1,101 @@
+package probleme1;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class QuadraticSpacePerfectHashing<AnyType> 
+{
+	static int p = 46337;
+
+	int a, b;
+	AnyType[] items;
+
+	QuadraticSpacePerfectHashing()
+	{
+		a=b=0; items = null;
+	}
+
+	QuadraticSpacePerfectHashing(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	public void SetArray(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
+
+	public int Size()
+	{
+		if( items == null ) return 0;
+
+		return items.length;
+	}
+
+	public boolean containsKey(int key)
+	{
+		// A completer
+
+	}
+
+	public boolean containsValue(AnyType x )
+	{
+		// A completer
+
+	}
+
+	public void remove (AnyType x) {
+		// A completer
+
+	}
+
+	public int getKey (AnyType x) {
+		// A completer
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	private void AllocateMemory(ArrayList<AnyType> array)
+	{
+		Random generator = new Random( System.nanoTime() );
+
+		if(array == null || array.size() == 0)
+		{
+			// A completer
+			return;
+		}
+		if(array.size() == 1)
+		{
+			a = b = 0;
+
+			// A completer			
+			return;
+		}
+
+		do
+		{
+			items = null;
+
+			// A completer
+
+		}
+		while( collisionExists( array ) );
+	}
+
+	@SuppressWarnings("unchecked")
+	private boolean collisionExists(ArrayList<AnyType> array)
+	{
+		// A completer
+
+		return false;
+	}
+	
+	public String toString () {
+		String result = "";
+		
+		// A completer
+		
+		
+		return result; 
+	}
+}
